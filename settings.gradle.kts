94dd22ff5a2cd9e8ef79253324613ee576bb242b
Inaki Villar <inaki.seri@gmail.com>
2024-09-06 14:51:30 -0700

    initial import

---
+++ settings.gradle.kts
@@ -0,0 +1,37 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("com.gradle.develocity") version "3.18"
+}
+
+develocity {
+    server = "https://ge.solutions-team.gradle.com"
+    allowUntrustedServer = true
+    buildScan {
+        uploadInBackground.set(false)
+        publishing { true}
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Application"
+include(":app")
+
+include(":mylibrary")
